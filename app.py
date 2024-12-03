9f92e51822c6bdb6b79974ae403ca8e5d665ed7c
polkadot-market <85949205+polkadot-market@users.noreply.github.com>
2024-12-03 14:32:11 +0000

    Update app.py
    Update for manual mode now Reddit API doesn't work.

--- app.py
+++ app.py
@@ -13,31 +13,18 @@ action: spam
 action_reason: "Domain on polkadot.js.org blocklist [{{match}}]"
 # PHISHBLOCKEND""")
 
-def update_automoderator(deny):
-    reddit = praw.Reddit()
-    subreddit = reddit.subreddit(str(subreddit_name))
-    wikipage = subreddit.wiki["config/automoderator"]
-    content_md = wikipage.content_md
-    begin_md = content_md.split("# PHISHBLOCKBEGIN")[0]
-    end_md = content_md.split('# PHISHBLOCKEND')[1]
-    middle_md = block_template.substitute(blocklist=", ".join(deny))
-    new_content = begin_md + middle_md + end_md
-    if new_content != content_md:
-        output = wikipage.edit(new_content, 'update blocklist')
-        print(f"Updated automoderator config {output}")
-    else:
-        print("Config is already up to date.")
+def print_block(deny):
+    # TODO: somehow check if content has changed
+    content = block_template.substitute(blocklist=", ".join(deny))
+    from pprint import pprint
+    pprint(content)
 
 with urlopen(url) as response:
     try:
         all = json.load(response)
         deny = all['deny']
+        print_block(deny)
+        print("-" * 10)
         print(f'{len(deny)} hosts on deny list.')
-        try:
-            import praw
-            print(f"Attempting to update {subreddit_name} config.")
-            update_automoderator(deny)
-        except ModuleNotFoundError:
-            print("You need to install praw if you want to update automoderator config")
     except json.JSONDecodeError:
         print("JSON malformed from GitHub")
